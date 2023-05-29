1a46587e05ca79cb00c24aa24ff008fb394ed0cd
dev-morph <112484043+dev-morph@users.noreply.github.com>
2023-05-29 12:56:54 +0900

    탐지 범위 등 custom properties 세팅 가능 구현

--- src/components/TestPage.vue
+++ src/components/TestPage.vue
@@ -4,10 +4,11 @@
 			<button @click="clickHandler">
 				{{ animating ? 'Stop Car' : 'Move Car' }}
 			</button>
-			<button @click="gobackHandler">뒤로 가기</button>
+
+			<button @click="gobackHandler">Go Back</button>
 		</div>
 
-		<p>CURRENT POSITION: {{ mousePosition }}</p>
+		<p>CLICK POSITION: {{ mousePosition }}</p>
 
 		<div class="map" ref="map"></div>
 	</div>
@@ -32,7 +33,6 @@ import { esaseIn } from 'ol/easing'
 
 export default {
 	name: 'TestPage',
-
 	setup() {
 		const testGeoJson = {
 			type: 'FeatureCollection',
@@ -45,8 +45,7 @@ export default {
 					},
 					properties: {
 						heading: 1.3,
-
-						radius: 50,
+						radius: 10,
 					},
 				},
 
@@ -56,53 +55,49 @@ export default {
 						type: 'Point',
 						coordinates: [126.92360304723802, 37.52956814419788],
 					},
+
 					properties: {
 						heading: 1.5,
-						radius: 60,
+						radius: 20,
 					},
 				},
+
 				{
 					type: 'Feature',
-
 					geometry: {
 						type: 'Point',
-
 						coordinates: [126.92179651968831, 37.5314476361026],
 					},
 
 					properties: {
 						heading: 1.7,
-						radius: 50,
+						radius: 30,
 					},
 				},
 
 				{
 					type: 'Feature',
-
 					geometry: {
 						type: 'Point',
-
 						coordinates: [126.92193554791005, 37.531841398589975],
 					},
 
 					properties: {
 						heading: 1.8,
-						radius: 70,
+						radius: 40,
 					},
 				},
 
 				{
 					type: 'Feature',
-
 					geometry: {
 						type: 'Point',
-
 						coordinates: [126.92551017508136, 37.53078910219364],
 					},
 
 					properties: {
 						heading: 1.9,
-						radius: 80,
+						radius: 20,
 					},
 				},
 
@@ -111,36 +106,28 @@ export default {
 
 					geometry: {
 						type: 'Point',
-
 						coordinates: [126.92512160311284, 37.53048855990768],
 					},
 
 					properties: {
 						heading: 2.1,
-						radius: 90,
+						radius: 30,
 					},
 				},
 			],
 		}
 
 		const animating = ref(false)
-
 		const map = ref(null)
-
 		const mousePosition = ref([])
-
 		const center = fromLonLat([126.9251405697578, 37.53033241217628])
-
 		const styles = {
 			Point: new Style({
 				image: new CircleStyle({
 					radius: 10,
-
 					fill: new Fill({ color: [160, 51, 255, 1] }),
-
 					stroke: new Stroke({
 						color: [160, 51, 255, 1],
-
 						width: 0.1,
 					}),
 				}),
@@ -149,20 +136,10 @@ export default {
 			Car: new Style({
 				image: new CircleStyle({
 					radius: 10,
-					fill: new Fill({ color: [255, 0, 0, 1] }),
+					fill: new Fill({ color: [255, 0, 0, 0.7] }),
 					stroke: new Stroke({
-						color: [160, 51, 255, 1],
-						width: 5,
-					}),
-				}),
-			}),
-			NewCar: new Style({
-				image: new CircleStyle({
-					radius: 10,
-					fill: new Fill({ color: [100, 0, 0, 1] }),
-					stroke: new Stroke({
-						color: [160, 51, 255, 1],
-						width: 5,
+						color: [160, 51, 255, 0.7],
+						width: 3,
 					}),
 				}),
 			}),
@@ -170,15 +147,6 @@ export default {
 			MultiLineString: new Style({
 				stroke: new Stroke({
 					width: 3,
-
-					color: [160, 51, 255, 1],
-				}),
-			}),
-
-			line: new Style({
-				stroke: new Stroke({
-					width: 3,
-
 					color: [160, 51, 255, 1],
 				}),
 			}),
@@ -198,6 +166,8 @@ export default {
 			return feature
 		})
 
+		// 스타일 적용 함수
+
 		function styleHandler(feature) {
 			const customType = feature.values_.type
 
@@ -212,35 +182,55 @@ export default {
 			if (!animating.value) {
 				startAnimation()
 			} else {
-				stopAnimation()
+				pauseAnimation()
 			}
 		}
 
 		const duration = 1000
+
 		let start
-		let currentIndex = 0
-		let distance = 0 // Animation Method
+
+		const currentIndex = ref(0)
 
 		function moveFeature(event) {
 			const frameState = event.frameState
 			const elapsedTime = frameState.time - start
-			const from = features[currentIndex].getGeometry().getCoordinates()
-			const to =
-				currentIndex === features.length - 1
-					? features[0].getGeometry().getCoordinates()
-					: features[currentIndex + 1].getGeometry().getCoordinates()
+			const currentFeature = features[currentIndex.value]
+			const nextFeature = features[currentIndex.value + 1]
+			const from = currentFeature.getGeometry().getCoordinates()
+
+			let to
+			let carStyle
+
+			if (currentIndex.value === features.length - 1) {
+				to = features[0].getGeometry().getCoordinates()
+
+				carStyle = styles['Car']
+			} else {
+				to = nextFeature.getGeometry().getCoordinates()
+				carStyle = new Style({
+					image: new CircleStyle({
+						radius: nextFeature.getProperties().properties.radius,
+						fill: new Fill({ color: [255, 0, 0, 0.7] }),
+						stroke: new Stroke({
+							color: [160, 51, 255, 0.7],
+							width: 3,
+						}),
+					}),
+				})
+			}
 
 			// duration에 도달 했다면, 이는 도착 위치에 도달 했다는 뜻.
-			// currentIndex를 하나 늘리고, 다시 반복하면 된다.
-			// 단, currentIndex가 features.length에 도달하기 전까지 postrender이벤트를 멈추면 안되고,
+			// currentIndex.value를 하나 늘리고, 다시 반복하면 된다.
+			// 단, currentIndex.value가 features.length에 도달하기 전까지 postrender이벤트를 멈추면 안되고,
 			// 도달 했을 때에만 postrender이벤트를 unlisten 하기 위해, stopAnimation()을 호출 한다.
+
 			if (elapsedTime >= duration) {
-				if (++currentIndex >= features.length) {
+				if (++currentIndex.value >= features.length) {
 					stopAnimation()
 					carPosition.setCoordinates(from)
 					return
 				}
-				console.log('curIdx', currentIndex, features.length)
 				start = Date.now()
 				vectorLayer.on('postrender', moveFeature)
 			}
@@ -248,7 +238,9 @@ export default {
 			const vectorContext = getVectorContext(event)
 
 			// Important
+
 			// 정해진 시간(duration)에서 실제 지나간 시간 비율로 현재 좌표를 구해 애니메이션 효과를 넣어준다.
+
 			const elapsedRatio = elapsedTime / duration
 			const currentPosition = new Point([
 				from[0] + (to[0] - from[0]) * elapsedRatio,
@@ -256,33 +248,45 @@ export default {
 			])
 
 			console.log('currentPosition', currentPosition.getCoordinates())
-			console.log('currentIndex', currentIndex)
 
 			// 구한 현재 좌표와 스타일을 적용시킨다.
+
 			carPosition.setCoordinates(currentPosition.getCoordinates())
+
 			// 여기서 heading 값을 이용하여
-			// styleMap['car'].getImage().setRotation(flipRotation)
+			// styles['car'].getImage().setRotation(flipRotation)
 			// 형식으로 rotation 값을 변경 해 줄 수 있다.
-			vectorContext.setStyle(styles.Car)
+
+			vectorContext.setStyle(carStyle)
 			vectorContext.drawGeometry(carPosition)
 			olMap.render()
 		}
 
 		function gobackHandler() {
-			console.log('currentIndex', currentIndex)
-			// if (currentIndex === 0) {
-			// 	startAnimation()
-			// 	stopAnimation()
-			// } else {
-			// 	currentIndex--
-			// 	console.log(currentIndex)
-			// }
+			console.log('currentIndex.value', currentIndex.value)
+			if (currentIndex.value === 0) {
+				startAnimation()
+				stopAnimation()
+			} else {
+				currentIndex.value--
+
+				console.log(currentIndex.value)
+			}
 		}
+
 		function startAnimation() {
 			animating.value = true
 			start = Date.now()
 			vectorLayer.on('postrender', moveFeature)
 			carFeature.setGeometry(null)
+		}
+
+		function pauseAnimation() {
+			animating.value = false
+			start = Date.now()
+			vectorLayer.un('postrender', moveFeature)
+			//초기화
+			carFeature.setGeometry(carPosition)
 		}
 
 		function stopAnimation() {
@@ -291,8 +295,7 @@ export default {
 			vectorLayer.un('postrender', moveFeature)
 			//초기화
 			carFeature.setGeometry(carPosition)
-			currentIndex = 0
-			distance = 0
+			currentIndex.value = 0
 		}
 
 		const multiLine = new MultiLineString([
@@ -308,15 +311,15 @@ export default {
 		})
 
 		const movingSource = new VectorSource({
-			// features: [...features, carFeature, lineFeature],
 			features: [lineFeature, carFeature],
 		}) //Vector layer 선언
 
 		const vectorLayer = new VectorLayer({
 			source: movingSource,
-
 			style: styleHandler,
 		})
+
+		// Map Config
 
 		let olMap
 
@@ -324,26 +327,32 @@ export default {
 			olMap = new OlMap({
 				target: map.value,
 
+				// override default controls to disappear
+
 				controls: defaults({
 					attribution: false,
-
 					zoom: false,
-
 					rotate: false,
 				}),
 
+				// add layers to Map Object
+
 				layers: [new OlLayerTile({ source: new OSM() }), vectorLayer],
+
+				// add View Object that determines how and where to show in Map Object
+				// e.g. center, zoom-level, projection
 
 				view: new OlView({
 					center: center, // 여의도 좌표
-
 					zoom: 17,
 				}),
 			})
 
+			// if you want to add event on Map Object,
+			// you can add event like below,
+
 			olMap.on('click', (event) => {
 				const currentPosition = toLonLat(event.coordinate)
-
 				mousePosition.value = currentPosition
 			})
 		})
@@ -366,25 +375,22 @@ export default {
 <style scoped>
 .map__wrapper {
 	width: 100%;
-
 	height: 100%;
-
 	display: flex;
-
 	flex-direction: column;
-
 	align-items: center;
 }
 
 .map {
 	width: 100%;
-
 	height: 100%;
 }
+
 .btn__wrapper {
 	display: flex;
 	gap: 2rem;
 }
+
 button {
 	padding: 0.3rem 0.5rem;
 	border: none;
@@ -392,6 +398,7 @@ button {
 	border-radius: 0.5rem;
 	color: white;
 }
+
 button:hover {
 	background-color: #42d392;
 	cursor: pointer;
